7bdde0d0d255d826694a853409bbf33e2a28883d
Mel <mel.ndrw@gmail.com>
2020-07-16 08:58:18 +0800

    Change .gitignore to specific and add .ENV

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
-
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/userDB', {
@@ -17,10 +17,8 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Imyoursecretkeepmeunknown"
-
 userSchema.plugin(encrypt, {
-  secret: secret,
+  secret: process.env.SECRET,
   encryptedFields: ['password']
 });
 
